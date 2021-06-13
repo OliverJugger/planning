dfc93927fe1a59254ee314ed4ce18272e2b2f6d4
Olivier Mignot <olivier.mignot@ac-aix-marseille.fr>
2021-06-13 21:07:46 +0200

    Ajout export semaine pair et impaire

--- planning.sql
+++ planning.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 13 juin 2021 à 16:51
+-- Généré le :  Dim 13 juin 2021 à 19:06
 -- Version du serveur :  5.7.23
 -- Version de PHP :  7.2.10
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `planningImpair`
+-- Base de données :  `planning`
 --
 
 -- --------------------------------------------------------
@@ -81,7 +81,7 @@ CREATE TABLE IF NOT EXISTS `lundi_aprem` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `lundi_aprem`
@@ -91,8 +91,8 @@ INSERT INTO `lundi_aprem` (`id`, `nom`) VALUES
 (6, 'Benoit'),
 (7, 'Monique'),
 (8, 'Laeticia'),
-(9, 'Audrey'),
-(12, 'Sylvie');
+(15, 'Audrey'),
+(16, 'Sylvie');
 
 -- --------------------------------------------------------
 
